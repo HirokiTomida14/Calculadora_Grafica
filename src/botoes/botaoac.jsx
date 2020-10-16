0950017668864bd2b46fc6a57f1d8081febed17f
Mauricio Hiroki Tomida <hiroki.tomida@usp.br>
2020-10-16 18:05:07 -0300

    Calculadora Básica

--- src/botoes/botaoac.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../App.css';
-
-export const botao_AC = (props) => (
-    <div className="Button" onClick={props.handleClear}>
-        {props.children}
-    </div>
-)
